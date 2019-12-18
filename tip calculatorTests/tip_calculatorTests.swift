3934836b8675500d01836d7faabaaf8138861ef7
Baraa Hegazy <hegazyba@msu.edu>
2019-12-18 00:36:39 -0500

    Initial Commit

---
+++ tip calculatorTests/tip_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  tip_calculatorTests.swift
+//  tip calculatorTests
+//
+//  Created by Baraa Hegazy on 12/17/19.
+//  Copyright © 2019 BaraaHegazy. All rights reserved.
+//
+
+import XCTest
+@testable import tip_calculator
+
+class tip_calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
